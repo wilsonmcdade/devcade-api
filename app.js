6695b8fdba35479735575aa847f1f651aebdf2eb
Benjamin Piro <brp8396@rit.edu>
2022-10-01 01:34:45 -0400

    Removed unnecessary dependencies and tried to add connection to postgres server

--- app.js
+++ app.js
@@ -1,49 +1,39 @@
 const config = require('./utils/config');
 const express = require('express');
 const app = express();
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
-const mongoose = require('mongoose');
+const pg = require('pg');
 
 // Routers
-const notesRouter = require('./controllers/notes');
-const contactRouter = require('./controllers/contact');
-const authRouter = require('./controllers/auth');
+//const notesRouter = require('./controllers/notes');
 
-logger.info(`connecting to mongodb+srv://bean217:${config.MONGODB_PASS}@highwireshop.v7swd.mongodb.net/?retryWrites=true&w=majority`);
 
-mongoose.connect(`mongodb+srv://bean217:${config.MONGODB_PASS}@highwireshop.v7swd.mongodb.net/?retryWrites=true&w=majority`)
-    .then(() => {
-        logger.info(`mongodb://${config.MONGODB_USER}@${config.MONGODB_SERVER}`);
-    })
-    .catch((error) => {
-        logger.error('error connecting to MongoDB:', error.message);
-    });
-// mongoose.Promise is depractated, override with global.Promise
-mongoose.Promise = global.Promise;
+// ##### POSTGRES CONNECTION #####
+const client = new pg.Client({
+    host: config.PSQL_URI,
+    user: config.PSQL_USER,
+    port: config.PORT,
+    password: config.PSQL_PASS,
+    database: config.PSQL_USER,
+    ssl: true
+});
 
-app.use(cors({
-    // origin domain is the URL of the client
-    origin: 'http://localhost:3000',
-    credentials: true
-}));
-app.use(cookieParser());
+client.connect().catch(e => console.log(e));
+
+logger.info(`connecting to {
+    host: ${client.host},
+    user: ${client.user},
+    port: ${client.port},
+    password: ${client.password},
+    database: ${client.database}
+}`);
+
 app.use(express.static('build'));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
-// start TEMPORARY
-//app.get('/', (req, res) => {
-//    console.log(req);
-//    res.send('<h1>Highwire Shop</h1>');
-//});
-// end TEMPORARY
-
-// app.use('/api/notes', notesRouter);
-app.use('/api/contact', contactRouter);
-app.use('/api/auth', authRouter);
+//app.use('/api/notes', notesRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- utils/config.js
+++ utils/config.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 
 const PORT = process.env.PORT;
-const MONGODB_USER = process.env.MONGODB_USER;
-const MONGODB_SERVER = process.env.MONGODB_SERVER;
-const MONGODB_PASS = encodeURIComponent(process.env.MONGODB_PASS);
+const PSQL_USER = process.env.PSQL_USER;
+const PSQL_PASS = encodeURIComponent(process.env.PSQL_PASS);
+const PSQL_URI = process.env.PSQL_URI;
 
 module.exports = {
     PORT,
-    MONGODB_USER,
-    MONGODB_SERVER,
-    MONGODB_PASS
+    PSQL_USER,
+    PSQL_PASS,
+    PSQL_URI
 };
