296f320c1b0d989d1c109bba599e06577434b212
Benjamin Piro <brp8396@rit.edu>
2022-10-06 16:10:53 -0400

    Figuing out how to upload zip files to the server

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const pg = require('pg');
+const gamesRouter = require('./controllers/games');
 
 // Routers
 //const notesRouter = require('./controllers/notes');
@@ -33,6 +34,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 //app.use('/api/notes', notesRouter);
+app.use('/api/games', gamesRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
