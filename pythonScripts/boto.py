fdd0ca5a8fb1e1b5cfaf4fe40ebc6bab1ea65617
Benjamin Piro <brp8396@rit.edu>
2022-11-04 08:49:23 -0400

    Removed s3 bucket access credentials from boto3.py

--- pythonScripts/boto.py
+++ pythonScripts/boto.py
@@ -3,18 +3,21 @@ from botocore.exceptions import ClientError
 import sys
 import os
 from dotenv import dotenv_values
-# Retrieve the list of existing buckets
 
-access_key = "devcade2022AccessKey8acb00449528cd9f93bd8d1f21558918511048ea498f77a721be025077b29cd7"
-secret_key = "devcade2022SecretKeyd35460c908e215c3612ef82edd8519ead606ed05b0dedc7d3715f52afc7ae1b8"
+# Retrieve the list of existing buckets
+config = dotenv_values('.env')
 
 def upload_file(file_name, bucket, object_name=None):
+    # get dotenv values
+    access_key = config['S3_ACCESSKEYID']
+    secret_key = config['S3_SECRETACCESSKEY']
+    endpoint = config['S3_ENDPOINT']
     # If s3 object_name was not specified
     if object_name is None:
         object_name = os.path.basename(file_name)
 
     # Upload the file
-    s3_client = boto3.client('s3', endpoint_url='https://s3.csh.rit.edu', aws_access_key_id=access_key, aws_secret_access_key=secret_key)
+    s3_client = boto3.client('s3', endpoint_url=endpoint, aws_access_key_id=access_key, aws_secret_access_key=secret_key)
     response = s3_client.upload_file(file_name, bucket, object_name)
 
 
@@ -26,11 +29,10 @@ if __name__ == '__main__':
         print('Missing Arguments', file=sys.stderr)
     else:
         try:
-            # upload game zip file
-            upload_file(f'uploads/{sys.argv[1]}/{sys.argv[1]}.zip', 'devcade-games', f'{sys.argv[1]}/{sys.argv[1]}.zip')
+            upload_file(f'uploads/{sys.argv[1]}/{sys.argv[1]}.zip', config['S3_GAMES_BUCKET'], f'{sys.argv[1]}/{sys.argv[1]}.zip')
             # upload game icon and banner files
-            upload_file(f'uploads/{sys.argv[1]}/{sys.argv[2]}', 'devcade-games', f'{sys.argv[1]}/{sys.argv[2]}')
-            upload_file(f'uploads/{sys.argv[1]}/{sys.argv[3]}', 'devcade-games', f'{sys.argv[1]}/{sys.argv[3]}')
+            upload_file(f'uploads/{sys.argv[1]}/{sys.argv[2]}', config['S3_GAMES_BUCKET'], f'{sys.argv[1]}/{sys.argv[2]}')
+            upload_file(f'uploads/{sys.argv[1]}/{sys.argv[3]}', config['S3_GAMES_BUCKET'], f'{sys.argv[1]}/{sys.argv[3]}')
             print("success") 
         except ClientError as e:
             print(e, file=sys.stderr)
