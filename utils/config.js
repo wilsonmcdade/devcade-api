5b1cb03a82cec644b87c6f76b1b7f9c34ac8345f
Benjamin Piro <brp8396@rit.edu>
2022-11-10 20:28:34 -0500

    Attempting to rewrite python s3utils in node

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({path: "../.env"});
 
 const API_PORT = process.env.API_PORT;
 
@@ -10,6 +10,9 @@ const PSQL_URI = process.env.PSQL_URI;
 const S3_ACCESSKEYID = process.env.S3_ACCESSKEYID;
 const S3_SECRETACCESSKEY = process.env.S3_SECRETACCESSKEY;
 const S3_ENDPOINT = process.env.S3_ENDPOINT;
+const S3_REGION = process.env.S3_REGION;
+const S3_GAMES_BUCKET = process.env.S3_GAMES_BUCKET;
+const S3_SAVES_BUCKET = process.env.S3_SAVES_BUCKET;
 
 module.exports = {
     API_PORT,
@@ -19,5 +22,8 @@ module.exports = {
     PSQL_URI,
     S3_ACCESSKEYID,
     S3_SECRETACCESSKEY,
-    S3_ENDPOINT
+    S3_ENDPOINT,
+    S3_REGION,
+    S3_GAMES_BUCKET,
+    S3_SAVES_BUCKET
 };
